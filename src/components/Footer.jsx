e32ee42a785b36520c19efb088a8524548f58817
Simon Green <simonisaacgreen@icloud.com>
2019-10-14 14:50:42 +0100

    first commit with initial layout and reach router installed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Footer content</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p>Header content</p>
+    </div>
+  );
+};
+
+export default Header;
