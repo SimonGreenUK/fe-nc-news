0cf0046ff4560062950efcddd79644c25195ea0f
Simon Green <simonisaacgreen@icloud.com>
2019-10-17 09:03:04 +0100

    minor layout tweak for user login panel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,29 @@ import { Link } from '@reach/router';
 import no_img from '../assets/images/no_img.png';
 import DesktopMenu from './DesktopMenu';
 import HamburgerIcon from './HamburgerIcon';
+import styled from 'styled-components';
+
+const UserImg = styled.img`
+  max-width: 50px;
+  height: auto;
+`;
 
 const Header = props => {
   return (
     <header className="header">
       <div className="header-current-user-info">
-        {/* <img
-        src={props.loggedInUser.avatar_url}
-        onError={e => {
-          e.target.onerror = null;
-          e.target.src = no_img;
-        }}
-        alt="user avatar"
-      /> */}
-        <span>USER IMG HERE - {props.loggedInUser.username}</span>
-        <Link to={`/change-user`}>Change User</Link>
+        <div className="header-current-user-info-wrapper">
+          <Link to={`/change-user`}>Change User</Link>
+          <span>{props.loggedInUser.username}</span>
+          <UserImg
+            src={props.loggedInUser.avatar_url}
+            onError={e => {
+              e.target.onerror = null;
+              e.target.src = no_img;
+            }}
+            alt="user avatar"
+          />
+        </div>
       </div>
       <div className="header-title">
         <h1>NC NEWS</h1>
