d0e6833cc305343b4f4ae5d69dd59c5211a73fce
Simon Green <simonisaacgreen@icloud.com>
2019-10-17 09:16:07 +0100

    basic mobile nav styling

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -12,11 +12,33 @@ const MobileMenuWrapper = styled.div`
   justify-content: center;
   height: 100vh;
   position: fixed;
-  right: 55px;
-  background: lightgray;
+  /* right: 55px; */
+  background: black;
   overflow: hidden;
   transition: width 0.3s ease - out;
   text-align: left;
+  color: white;
+  font-size: 2rem;
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const MobileMenuInnerWrapper = styled.div`
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 
 class MobileMenu extends React.Component {
@@ -33,7 +55,7 @@ class MobileMenu extends React.Component {
       >
         <>
           {!this.state.isLoading && (
-            <div>
+            <MobileMenuInnerWrapper>
               <li>
                 <Link to={'/articles'} onClick={this.props.toggleMobileMenu}>
                   All
@@ -51,7 +73,7 @@ class MobileMenu extends React.Component {
                   </li>
                 );
               })}
-            </div>
+            </MobileMenuInnerWrapper>
           )}
           {this.state.isLoading && (
             <h2>
