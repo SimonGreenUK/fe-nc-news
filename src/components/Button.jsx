7f136437b967f962d50f1c59d99b4b1825760916
Simon Green <simonisaacgreen@icloud.com>
2019-10-21 20:56:26 +0100

    reduce button font weight but increase text size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ButtonPrimary = styled.button`
-  font-size: 0.5rem;
+  font-size: 0.7rem;
   font-family: inherit;
-  font-weight: 900;
+  /* font-weight: 900; */
   border: 1px solid var(--turquoise-main);
   border-radius: 2rem;
   text-transform: uppercase;
-  width: 7rem;
+  width: 9rem;
   height: 2rem;
   margin: 5px 10px 5px 0;
   transition: all 0.3s ease;
