9f87499afdfd71693f2f8b34d958def0fbfc50e1
Simon Green <simonisaacgreen@icloud.com>
2019-10-21 22:40:59 +0100

    removing minimum height from overall header

--- src/partials/header/Header.jsx
+++ src/partials/header/Header.jsx
@@ -9,7 +9,7 @@ const HeaderStyled = styled.header`
   grid-area: header;
   background-color: var(--black);
   color: white;
-  min-height: 160px;
+  /* min-height: 160px; */
 
   a {
     text-decoration: none;
