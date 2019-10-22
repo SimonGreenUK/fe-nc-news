4117687e0fe48ad0112af6cbffcf622c6b98619e
Simon Green <simonisaacgreen@icloud.com>
2019-10-22 07:09:02 +0100

    make button narrower and text turquoise

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,11 +8,11 @@ const ButtonPrimary = styled.button`
   border: 1px solid var(--turquoise-main);
   border-radius: 2rem;
   text-transform: uppercase;
-  width: 9rem;
+  width: 7rem;
   height: 2rem;
   margin: 5px 10px 5px 0;
   transition: all 0.3s ease;
-  color: inherit;
+  color: var(--turquoise-main);
   :disabled {
     color: var(--light-gray);
   }
